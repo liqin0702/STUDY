53c2c41edc154e2ea3e2654fc06c044158dbe19d
Li_pc <liqin0702@126.com>
2019-05-05 19:05:39 +0800

    5.5

---
+++ 1.js
@@ -0,0 +1,49 @@
+{
+    "debug": true,
+    "delay": 0.25,
+    "gutter_theme": "Danish Royalty",
+    "lint_mode": "background",
+    "linters": {
+        "eslint": {
+            "@disable": false,
+            "args": [],
+            "excludes": [],
+        }
+    },
+    "no_column_highlights_line": false,
+    "paths": {
+        "linux": [],
+        "osx": [],
+        "windows": [""]
+    },
+    "show_marks_in_minimap": true,
+    "styles": [
+        {
+            "mark_style": "outline",
+            "priority": 1,
+            "scope": "region.yellowish markup.changed.sublime_linter markup.warning.sublime_linter",
+            "icon": "dot",
+            "types": [
+                "warning"
+            ]
+        },
+        {
+            "mark_style": "outline",
+            "priority": 1,
+            "scope": "region.redish markup.deleted.sublime_linter markup.error.sublime_linter",
+            "icon": "dot",
+            "types": [
+                "error"
+            ]
+        }
+    ],
+    "syntax_map": {
+        "html (django)": "html",
+        "html (rails)": "html",
+        "html 5": "html",
+        "javascript (babel)": "javascript",
+        "magicpython": "python",
+        "php": "html",
+        "python django": "python"
+    }
+}
