8b7d907bd731a8830cf3a401e6f0b92d6c6f10de
li-ubuntu-pycharm <liqin0702@126.com>
2019-07-21 20:55:24 +0800

    7.21

--- 09express/03express-curd/2.js
+++ 09express/03express-curd/2.js
@@ -1,4 +1,15 @@
-const a = 3
-const b = 2
-exports.a1 = a
-module.exports = b
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('hello')
+})
+
+
+server.on('connection', (req, res) => {
+  console.log('connected')
+})
+server.on('request', (req, res) => {
+  console.log('request')
+})
+server.listen(3000)
